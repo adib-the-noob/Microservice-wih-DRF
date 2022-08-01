9b8711347860f94e860ec2b278175c0fc0e7f958
Mohammed Adib <76277079+ADIB-the-NOOB@users.noreply.github.com>
2022-08-01 21:02:35 +0600

    Web Scraping Done!

--- scraper/helpers.py
+++ scraper/helpers.py
@@ -6,5 +6,10 @@ def get_all_stock_urls():
         page = 'https://stocks.zerodha.com/'
         soup = BeautifulSoup(requests.get(page).text, 'html.parser')
         print(soup.prettify())
+        for ul in soup.find_all('div',class_="index-page"):
+            for li in ul.find_all('li'):
+                anchor_tag = li.find('a')
+                print(anchor_tag['href'])
+                print(anchor_tag.text)
     except Exception as e:
         print(e)
